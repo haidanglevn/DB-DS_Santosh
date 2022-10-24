c1adcbc2eeaf8d0f3da99b567b7f1a5b4b4dae9b
haidanglevn <haidang.levn@gmail.com>
2022-10-24 14:37:30 +0300

    #

--- Practice_2410/practice/anagram.js
+++ Practice_2410/practice/anagram.js
@@ -5,8 +5,25 @@
 //such as cinema, formed from iceman.
 
 // Time complexity - O(n)
+function validAnagram(stringA, stringB) {
+  if (stringA.length != stringB.length) {
+    console.log(false);
+  } else {
+    cleanString(stringA, stringB);
+  }
+}
 
-validAnagram('', '') // true
-validAnagram('aaz', 'zza') // false
-validAnagram('anagram', 'nagaram') // true
-validAnagram("rat","car") // false) // false
+function cleanString(stringA, stringB) {
+  newStringA = stringA.split("").sort().join("");
+  newStringB = stringB.split("").sort().join("");
+  if (newStringA === newStringB) {
+    console.log(true);
+  } else {
+    console.log(false);
+  }
+}
+
+validAnagram("", ""); // true
+validAnagram("aaz", "zza"); // false
+validAnagram("anagram", "nagaram"); // true
+validAnagram("rat", "car"); // false) // false
